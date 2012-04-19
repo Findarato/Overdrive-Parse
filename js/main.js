010dd3a2b870398d8fc071486f8982b1fcf159a4
Joseph Harry <findarato@gmail.com>
2012-04-19 11:28:03 -0500

    possibly moving to a python back end solution.  Need to get posting working though

--- js/main.js
+++ js/main.js
@@ -5,10 +5,12 @@
 
 function getStatus(isbn){
 	var form =   $('<form method="post" action="proxy.php" target="postingIframe"></form>').css("display","none");
+	//var form =   $('<form method="post" action="http://incolsa.lib.overdrive.com/BANGSearch.dll?Type=FullText" target="postingIframe"></form>').css("display","none");
 	$('<input type="hidden" />').attr({name: 'FullTextCriteria', value: isbn}).appendTo(form);
-	$('<input type="hidden" />').attr({name: 'FullTextField', value: 'All'}).appendTo(form);
+	$('<input type="hidden" />').attr({name: 'FullTextField', value: 'ISBN'}).appendTo(form);
+	
 	$('<input type="submit" id="gobutton" title="Go" alt="Go" value="Go">').appendTo(form);
-	var iframe = $("<iframe/>",{"src":"","id":"postingIframe"}).css({"width":"400px","height":"400px"}).attr("name","postingIframe")
+	var iframe = $("<iframe/>",{"src":"","id":"postingIframe"}).css({"width":"800px","height":"800px"}).attr("name","postingIframe")
 	iframe.load(function(){
 		me = $(this);
 		
--- proxy.php
+++ proxy.php
@@ -1,39 +1,6 @@
 <?Php
-extract($_POST);
-
 //set POST variables
-$url = 'http://incolsa.lib.overdrive.com/BANGSearch.dll?Type=FullText';
-$agent= 'Mozilla/5.0 (X11; Linux i686; rv:11.0) Gecko/20100101 Firefox/11.0';
 
-//url-ify the data for the POST
-//foreach($_POST as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
-//rtrim($fields_string,'&');
+$_POST["FullTextCriteria"];
 
-//open connection
-$ch = curl_init();
-$fields = array(
-	$_POST["FullTextCriteria"],
-	$_POST["FullTextField"]
-);
 
-//set the url, number of POST vars, POST data
-curl_setopt($ch,CURLOPT_URL,$url);
-curl_setopt($ch,CURLOPT_POST,TRUE);
-curl_setopt($ch,CURLOPT_POSTFIELDS,$fields);
-
-//curl_setopt($ch, CURLOPT_HEADER, false); 	       // Don't return HTTP headers
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);   // Do return the contents of the call
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($ch, CURLOPT_VERBOSE, true);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_USERAGENT, $agent);
-
-//execute post
-$result = curl_exec($ch);
-print_r($_POST);
-$info = curl_getinfo($ch);
-
-print_r($info);
-print_r($result);
-//close connection
-curl_close($ch);
